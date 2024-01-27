f8786b435b4f81f36a23e586e6dd5c01968e1e98
LautaroCecchini <76130400+LautaroCecchini@users.noreply.github.com>
2024-01-27 19:09:44 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 window.onload = function() {
-	prepararBotones();
-	probarContenido();
+	prepararBotonesCategorias();
 }
 
 /*Funciones preparatorias*/
 
-function prepararBotones() {
+function prepararBotonesCategorias() {
 	var botones = document.getElementById("categorias").children;
 	for (var i=0; i < botones.length; i++) {
 		botones[i].onclick = function() {
@@ -13,6 +12,21 @@ function prepararBotones() {
 		}	
 	}
 }
+
+function prepararBotonesPortadas() {
+	var botones = document.getElementsByClassName("info");
+	for (var i = 0; i < botones.length; i++) {
+		botones[i].onclick = function () {
+			insertarContenido(this);
+		}
+	}
+}
+
+function prepararContenido(set_chosen) {
+	set_actual = set_chosen;
+}
+	
+	
 
 /* */
 
@@ -30,7 +44,7 @@ function prepararBotones() {
 /*Bien, esto funciona. Ahora, voy a tratar de definir una función para insertar las cuatro portadas
 usando esta como referencia.*/
 
-function insertarPortadas(set_chosen_covers) {
+function insertarPortadas(set_chosen) {
     var marcos = document.getElementsByClassName("marco");
 
     for (var i = 0; i < marcos.length; i++) {
@@ -39,7 +53,7 @@ function insertarPortadas(set_chosen_covers) {
         var imagen = document.createElement("img");
 
         imagen.setAttribute("class", "portada capa3");
-        imagen.setAttribute("src", set_chosen_covers[i]);
+        imagen.setAttribute("src", set_chosen[i]);
 
         marco.insertBefore(imagen, pie);
     }
@@ -49,22 +63,24 @@ function insertarPortadas(set_chosen_covers) {
 bucle for que itera sobre cada uno de los marcos. De esta forma, se ejecuta cuatro veces y le da a cada marco su portada
 correspondiente. ¡Fascinante! Voy a dejar el proceso registrado, ya que esto son como mis apuntes.*/
 
-function insertarInfo(set_chosen_info) {
+function insertarInfo(set_chosen) {
 	var marcos = document.getElementsByClassName("marco");
+	infos.length = 0; /*Reinicia el array a 0 cada vez que clickeo una categoría*/
 	
 	for (var i = 0; i < marcos.length; i++) {
 		var marco = marcos[i];
 		var pie = document.getElementsByClassName("pie")[i];
 		var info = document.createElement("div");
+		infos.push(info); /*Inserto cada div en el array para que tengan un índice correspondiente a su orden, que voy a usar luego en la función "insertarContenido"*/
 		info.setAttribute("class", "info capa2");
 		
 		var artista = document.createElement("p");
-		var artista_txt = document.createTextNode(set_chosen_info[i][0]);
+		var artista_txt = document.createTextNode(set_chosen[i][0]);
 		artista.setAttribute("class", "artista");
 		artista.appendChild(artista_txt);
 		
 		var titulo = document.createElement("p");
-		var titulo_txt = document.createTextNode(set_chosen_info[i][1]);
+		var titulo_txt = document.createTextNode(set_chosen[i][1]);
 		titulo.setAttribute("class", "titulo");
 		titulo.appendChild(titulo_txt);
 		
@@ -72,6 +88,7 @@ function insertarInfo(set_chosen_info) {
 		info.appendChild(titulo);
 		
 		marco.insertBefore(info, pie);
+		prepararBotonesPortadas();
 	}
 }
 
@@ -81,15 +98,46 @@ function probarContenido() {
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() { /* Acá hay algo que no entiendo. ¿Cómo le cambia el readyState al objeto? */
 		if (this.readyState == 4 && this.status == 200) { /* Esto quiere decir: "si la propiedad readyState de este objeto es 4 (el pedido fue hecho y la respuesta está lista) y la propiedad status es 200 (todo está bien)..." */
-			console.log(this.responseText);
+			var contenedor = document.getElementById("prueba");
+			contenedor.innerHTML = this.responseText; /*Estas líneas insertan el contenido a través del método innerHTML, que sirve para insertar grandes trozos de markup. */
 		}
 	};
 	
-	request.open("GET", "external-content/test.txt", true);
-	request.send();
-	
+	request.open("GET", "https://raw.githubusercontent.com/LautaroCecchini/FabFourAlbums/main/external-content/test.html?token=GHSAT0AAAAAACNN3TVWDN3OLQXQSVOCB2TQZNVKNPQ");
+	/* El método open recibe dos parámetros obligatorios: el tipo de pedido (en este caso "GET") y la ubicación del archivo */
+	request.send(); /* Y esto ejecuta el pedido */
 }
-		
+
+/* Esta función no va a ser llamada, pero queda ahí escrita como referencia. Ahora tengo que escribir la función 
+para insertar el contenido correspondiente a cada álbum */
+
+function insertarContenido(album_chosen) { /* Para esta función creé un array variable llamado "infos" al que voy a manipular dentro de la función "insertarInfo" */
+	var album_index = infos.indexOf(album_chosen);
+	for (i = 0; i < set_actual.length; i++) {
+		if (album_index != i) {
+			continue;
+		} else {
+			var request = new XMLHttpRequest();
+			request.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					var contenedor = document.getElementById("contenido");
+					contenedor.innerHTML = this.responseText;
+				}
+			};
+			
+			request.open("GET", set_actual[i]);
+			request.send();
+		}
+	}
+}
+
+/* */
+
+/*Array y variable móviles*/
+
+var infos = [];
+var set_actual = null;
+
 /* */
 
 /*Arrays de los sets*/
@@ -116,6 +164,12 @@ var set_espacio_info = [
 ["Julee Cruise", "Floating Into The Night"],
 ["Kate Bush", "Hounds of Love"],
 ["Aurora", "Infections of a Different Kind (Step 1)"]
+]
+var set_espacio_contenido = [
+"https://raw.githubusercontent.com/LautaroCecchini/FabFourAlbums/main/external-content/Titanic_Rising.html?token=GHSAT0AAAAAACNN3TVXGQBKMEIVGTXSHN34ZNVPURQ",
+"https://raw.githubusercontent.com/LautaroCecchini/FabFourAlbums/main/external-content/Floating_Into_The_Night.html?token=GHSAT0AAAAAACNN3TVWWV6LL5DEWEW3NTY4ZNVPTUA",
+"https://raw.githubusercontent.com/LautaroCecchini/FabFourAlbums/main/external-content/Hounds_of_Love.html?token=GHSAT0AAAAAACNN3TVX5VF7B6MJ73MHWJNWZNVLE4Q",
+"https://raw.githubusercontent.com/LautaroCecchini/FabFourAlbums/main/external-content/Infections_of_a_Different_Kind.html?token=GHSAT0AAAAAACNN3TVWWA4QDDY2V7I6VOMOZNVLE5A"
 ]
 
 var set_italian_shoes_portadas = ["images/covers/Tranquility Base Hotel & Casino.gif", "images/covers/Daddy's Home.gif", "images/covers/Parte De La Religión.gif", "images/covers/Blossom Dearie.gif"]
@@ -146,6 +200,7 @@ function activar(boton) {
 	else if (category == "espacio") {
 		insertarPortadas(set_espacio_portadas);
 		insertarInfo(set_espacio_info);
+		prepararContenido(set_espacio_contenido);
 	}
 	
 	else if (category == "italian-shoes") {
