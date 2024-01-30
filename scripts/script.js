1f272471bbad02c3b485d6ff67040d714fcb5f78
LautaroCecchini <76130400+LautaroCecchini@users.noreply.github.com>
2024-01-30 01:57:05 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -145,6 +145,7 @@ function insertarContenido(album_chosen) { /* Para esta función creé un array 
 				if (this.readyState == 4 && this.status == 200) {
 					var contenedor = document.getElementById("contenido");
 					contenedor.innerHTML = this.responseText;
+					contenedor.setAttribute("data-inserted", false);
 					var contenido = contenedor.querySelector("div");
 
 					if(contenedor.getAttribute("data-inserted") == false) { //Si el contenedor tiene el atributo "data-inserted" en false...
