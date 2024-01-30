678463ba640631f6eeea051592c9a4483d1b5625
LautaroCecchini <76130400+LautaroCecchini@users.noreply.github.com>
2024-01-30 00:43:06 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -137,7 +137,7 @@ function insertarContenido(album_chosen) { /* Para esta función creé un array 
 				if (this.readyState == 4 && this.status == 200) {
 					var contenedor = document.getElementById("contenido");
 					var contenido = this.responseText;
-					var cartucho = contenido.getElementsByTagName(div)[0];
+					var cartucho = contenido.getElementsByTagName("div")[0];
 					contenedor.innerHTML = contenido;
 					cartucho.classList.add("fade-in-active");
 				}
